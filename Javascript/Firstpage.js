4f20a262a2adab3826946af7d41adad289fac953
Googloo69 <afnanasif2003@gmail.com>
2022-10-20 17:59:12 +0500

    Js

---
+++ Javascript/Firstpage.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+function Firstpage(){
+    return(
+        <>
+      <img id="wasi" src="https://cdn.shopify.com/s/files/1/2277/5269/files/main_slider_desktop_14.jpg?v=1628230806" width="90%" height="60%" style={{marginLeft:"4rem"}}/>
+      <Button variant="outline-dark" style={{marginLeft: "40rem",marginTop: "-12rem"}}>SHOP NOW</Button>{' '}
+
+        {/* <span class="btn btn-outline-dark" class="shop">SHOP NOW</span> */}
+        </>
+    );
+}
+export default Firstpage;
